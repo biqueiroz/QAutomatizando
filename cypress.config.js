e3f8e6bb58fb20cfa8f260a26009107191141176
Bianca Queiroz <bianca.queiroz@webjump.com.br>
2023-06-14 17:28:02 -0300

    Update

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportWidth: 1440,
   viewportHeight: 900,
+  experimentalStudio: true,
 
   e2e: {
     baseUrl: 'https://magento2-demo.magebit.com',
+    experimentalRunAllSpecs: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
